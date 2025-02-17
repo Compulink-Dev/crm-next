fd5432fa26bebbf518a3bef4bad804cafe9b09f3
Compulink-Dev <devcompulink@gmail.com>
2025-02-17 13:01:46 +0200

    main

--- app/(main)/_components/Header.tsx
+++ app/(main)/_components/Header.tsx
@@ -80,7 +80,7 @@ function Header() {
               </li>
               <li>
                 <a
-                  href="https://techtrain.co.zw"
+                  href="/"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Company
@@ -88,7 +88,7 @@ function Header() {
               </li>
               <li>
                 <a
-                  href="https://next-shop-indol.vercel.app/"
+                  href="https://shop-next-ebon-nine.vercel.app/"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Marketplace
